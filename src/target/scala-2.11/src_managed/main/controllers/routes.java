c3061cdb9bdc9e27a030969b4e58742f273ad237
Jakub Nowak <jakub.nowak94@interia.pl>
2015-01-04 23:12:22 +0100

    Init application project.

---
+++ src/target/scala-2.11/src_managed/main/controllers/routes.java
@@ -0,0 +1,23 @@
+// @SOURCE:/home/jakub/workspace/workout-organizer/src/conf/routes
+// @HASH:b31e3ed4075748bc0576b3b7654b92b97f9d84ad
+// @DATE:Sun Jan 04 23:00:55 CET 2015
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+          
+
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+          
+}
+          
