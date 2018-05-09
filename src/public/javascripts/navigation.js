9f9f027d99d1ac53df7aecbab8da6b4b6609ca57
j-nowak <pl.jakub.nowak@gmail.com>
2018-05-08 21:36:34 -0700

    Clean up

--- src/public/javascripts/navigation.js
+++
@@ -1,8 +0,0 @@
-function activateNavbar(active) {
-	var navbars = ["nav-training", "nav-exercises", "nav-gyms", "nav-account"];
-	
-	navbars.forEach(function(entry) {
-		document.getElementById(entry).className = "";
-	});
-	document.getElementById(active).className = "active";
-}
